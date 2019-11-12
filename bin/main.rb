42052bba25f60b22abdc990e04ce7d6dc33f2078
Luis Puente <puente.luism@gmail.com>
2019-11-12 14:52:10 -0600

    Add user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,41 @@
 #!/usr/bin/env ruby
 
-puts "Hello world!"
+puts "Welcome to Tic-tac-toe!"
+
+puts "Input x player name:"
+x_player = gets.chomp
+puts "Welcome #{x_player}"
+
+puts "Input o player name:"
+o_player = gets.chomp
+puts "Welcome #{o_player}"
+
+puts "Let's start"
+
+turn = 0
+winner = false
+while turn < 9
+  puts "#{x_player}, it's your turn. Select a square from 1 to 9"
+  move = gets.chomp
+  turn += 1
+  puts "Your move was added to the board"
+  if winner
+    winner_name = x_player
+    break
+  end
+
+  puts "#{o_player}, it's your turn. Select a square from 1 to 9"
+  move = gets.chomp
+  turn += 1
+  puts "Your move was added to the board"
+  if winner
+    winner_name = o_player
+    break
+  end
+end
+
+if turn > 8
+    puts "It's a tie"
+else 
+    puts "The winner is #{winner_name}"
+end
