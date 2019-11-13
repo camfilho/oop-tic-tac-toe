9323a5ed5448eaf15d4e7aad8c5e6acd9ae86e8c
Luis Puente <puente.luism@gmail.com>
2019-11-13 15:01:23 -0600

    Add game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,46 @@
 #!/usr/bin/env ruby
 
-puts "Hello world!"
+require '../lib/tic_tac_toe.rb'
+
+puts "Welcome to Tic-tac-toe!"
+game = TicTacToe.new
+
+puts "Input x player name:"
+game.player_x.name = gets.chomp
+puts "Welcome #{game.player_x.name}"
+
+puts "Input o player name:"
+game.player_o.name = gets.chomp
+puts "Welcome #{game.player_o.name}"
+
+puts "Let's start"
+
+winner = false
+while game.turn < 9
+  begin
+    puts "It's #{game.current_player.name}'s turn. Select a square from #{game.board.avail_squares}"
+    move = gets.chomp.to_i
+    game.board.move(game.current_player.type, move) 
+  rescue StandardError => e
+    puts "Invalid move, try again"
+    retry
+  else
+    game.turn += 1
+    puts "Your move was added to the board"
+  end
+ 
+  winner = game.check_winner
+  
+  if winner
+    winner_name = game.current_player
+    break
+  else
+    game.switch_player
+  end
+end
+
+if game.turn > 8
+    puts "It's a tie"
+else 
+    puts "The winner is #{game.current_player.name}"
+end
