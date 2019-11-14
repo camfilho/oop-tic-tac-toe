799e749ee34542d97beaff93f41ba09852d41835
Luis Puente <puente.luism@gmail.com>
2019-11-14 15:06:25 -0600

    Add board tests

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -19,7 +19,6 @@ RSpec.describe Users do
 NoMethodError)
       end
     end
-    #subject { Users.new('Carlos', 'x') }
     context 'When I create a new User' do
       subject { described_class.new('Luis', 'o') }
       it 'returns Luis when I call by the name' do
@@ -32,4 +31,81 @@ NoMethodError)
     end
   end
 
+end
+
+RSpec.describe Board do
+  describe '#initialize' do
+    context 'When I create a new board' do
+      subject { described_class.new }
+      it 'returns the empty board with 1..9' do
+        expect(subject.board).to eql((1..9).to_a)
+      end
+
+      it 'returns the available spots, starting with 1..9' do
+        expect(subject.avail_squares).to eql(subject.board)
+      end
+    end
+  end
+  
+  describe '#rows' do
+    context 'When I display the rows' do
+      subject {described_class.new}
+      it 'returns the 3 set of rows in the tic-tac-toe game' do
+        expect(subject.rows).to eql([[1, 2, 3], [4, 5, 6], [7 ,8, 9]])
+      end
+    end
+  end  
+
+  describe '#columns' do
+    context 'When I display the columns' do
+      subject {described_class.new}
+      it 'returns the 3 sets of columns in the tic-tac-toe game' do
+        expect(subject.columns).to eql([[1, 4, 7] , [2, 5, 8] , [3, 6, 9]])
+      end
+    end
+  end
+
+  describe '#diagonals' do
+    context 'When I display the diagonals' do
+      subject {described_class.new}
+      it 'returns the 2 sets of diagonals in the tic-tac-toe game' do
+        expect(subject.diagonals).to eql([[1, 5, 9], [3, 5, 7]])
+      end
+    end
+  end
+
+  describe '#move' do
+    context 'When empty' do
+      subject {described_class.new}
+      it 'returns the new array containing the move' do
+        subject.move("o", 4)
+        subject.move("x", 3)
+        expect(subject.board).to eql ([1, 2, "x", "o", 5, 6, 7, 8, 9])      
+      end
+    end
+    context 'When not empty' do
+      subject {described_class.new}
+      it 'raise an exception if position taken' do
+        subject.move("o", 4)
+        expect { subject.move("x", 4) }.to raise_exception{'Wrong coordinates'}
+      end
+    end
+  end
+
+  describe '#empty?' do
+    context "When value is not valid" do
+      subject {described_class.new}
+      it 'will return false' do
+        expect(subject.empty?(99)).to eql(false)
+        expect(subject.empty?('a')).to eql(false)
+      end
+    end
+    context "When value is valid" do
+      subject {described_class.new}
+      it 'will return true' do
+        expect(subject.empty?(9)).to eql(true)
+        expect(subject.empty?(2)).to eql(true)
+      end
+    end
+  end
 end
