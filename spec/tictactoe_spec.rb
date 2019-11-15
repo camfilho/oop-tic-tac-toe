e2ba6c965581d8266e2848d86c3d0d9ed39281a0
Luis Puente <puente.luism@gmail.com>
2019-11-15 12:41:33 -0600

    Fix lint errors

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,35 +1,30 @@
+# frozen_string_literal: true
 
 require './lib/users'
 require './lib/board'
 
 RSpec.describe Users do
-
   describe '#initialize' do
-
-
     context 'When I change the name or type of the user' do
       subject { described_class.new('Luis', 'o') }
       it 'returns the new name after call by name' do
-        subject.name = "Carlos"
-        expect(subject.name).to eql("Carlos")
+        subject.name = 'Carlos'
+        expect(subject.name).to eql('Carlos')
       end
 
-      it "should raise an error when changing type" do
-        expect { subject.type = "i" }.to raise_error(
-NoMethodError)
+      it 'should raise an error when changing type' do
+        expect { subject.type = 'i' }.to raise_error(NoMethodError)
       end
     end
-    #subject { Users.new('Carlos', 'x') }
     context 'When I create a new User' do
       subject { described_class.new('Luis', 'o') }
       it 'returns Luis when I call by the name' do
-       expect(subject.name).to eql('Luis')
+        expect(subject.name).to eql('Luis')
       end
 
       it 'returns o when I call by the type' do
-       expect(subject.type).to eql('o')
+        expect(subject.type).to eql('o')
       end
     end
   end
-
 end
